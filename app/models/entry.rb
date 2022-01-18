e4bfdd730c88eed805dd0ff00ccbf8f0ede817b2
Nick Eikenberg <nickeikenberg95@gmail.com>
2022-01-18 15:48:22 -0500

    Generated bugs scaffold

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,6 @@
 class Entry < ApplicationRecord
+  belongs_to :category
+  validates :calories, :proteins, :carbs, :fats, :meal_type, :category_id, presence: true
 
   def day
     self.created_at.strftime("%b %e, %Y")
